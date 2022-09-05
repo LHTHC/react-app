0c36faa3ee71ae8c7f3a2ce383e557b60d634705
LHTHC <starfall1011@gmail.com>
2022-09-05 20:11:44 +0300

    added expences components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Expences from './components/Expences';
+
 function App() {
+  const expenses = [
+    { title: 'Car insurance', amount: 250.32, date: new Date(2021, 2, 23) },
+    { title: 'Books', amount: 160.38, date: new Date(2021, 3, 8) },
+    { title: 'Courses', amount: 1232, date: new Date(2021, 8, 6) },
+    { title: 'Toilet Paper', amount: 124, date: new Date(2020, 4, 14) },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h1>Hello, react!</h1>
+      <Expences items={expenses} />
     </div>
   );
 }
